8c41d33820c0310bf5ea0d8a4a682e604c690000
Noah <njhamann@gmail.com>
2023-09-25 08:56:32 -0500

    config

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+# Install nvim
+
+`sudo apt install neovim`
+
+or 
+
+`brew install neovim`
+
+then run
+
+`git clone https://github.com/nvim-lua/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim`
+
 # kickstart.nvim
 
 https://github.com/kdheepak/kickstart.nvim/assets/1813121/f3ff9a2b-c31f-44df-a4fa-8a0d7b17cf7b
--- init.lua
+++ init.lua
@@ -536,4 +536,12 @@ cmp.setup {
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- vim: ts=2 sts=2 sw=2 etc
+
+-- Below is my custom config
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
